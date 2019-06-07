82535dd37b82279081cc288dec161141408296b0
Zach Love <abc811@gmail.com>
2019-06-07 16:27:00 -0400

    Basic Setup
     - Added cinnamon.js stuff as per DM's commando template
     - Added framework for command structure
     - Added gitignore for secure.json and DS_Store

--- cinnamon.js
+++ cinnamon.js
@@ -1,6 +1,33 @@
-const Commando = require('discord.js-commando');
-const Discord = require('discord.js');
 const secure = require('./secure.json');
+const Commando = require('discord.js-commando');
 const path = require('path');
+const sqlite = require('sqlite');
 
-const Aeiou = new Commando.Client({});
+const Cinnamon = new Commando.Client({
+	owner: '147604925612818432',
+	commandPrefix: '!',
+	unknownCommandResponse: false,
+	disableEveryone: true,
+});
+
+Cinnamon.registry
+	.registerGroups([
+		['core', 'Core commands'],
+		['some', 'Some group'],
+		['other', 'Some other group'],
+	])
+	.registerDefaultTypes()
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+a
+Cinnamon.on('ready', () => {
+	console.log('lets do it');
+});
+
+
+Cinnamon.setProvider(
+	sqlite.open(path.join(__dirname, 'settings.sqlite3')).then((settingsProvider) => new Commando.SQLiteProvider(settingsProvider))
+).catch(console.error);
+
+Cinnamon.login(secure.discordAPIKey);
