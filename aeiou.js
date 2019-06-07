976ed8f7fd337ff27a8e3f092080c4f74b908de9
Zach Love <abc811@gmail.com>
2019-06-07 16:06:58 -0400

    Initial Commit
     - Added Discord.js and Commando
     - Added aeiou.js and secure.json

---
+++ aeiou.js
@@ -0,0 +1,6 @@
+const Commando = require('discord.js-commando');
+const Discord = require('discord.js');
+const secure = require('./secure.json');
+const path = require('path');
+
+const Aeiou = new Commando.Client({});
