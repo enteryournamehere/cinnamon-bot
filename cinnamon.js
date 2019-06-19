31323f2facb7cdac57f5431cf5bfa56073dc436d
Kuwar Nagpal <kuwar.nagpal@gmail.com>
2019-06-19 02:14:12 -0400

    Removed references to secure.json

--- cinnamon.js
+++ cinnamon.js
@@ -1,7 +1,5 @@
-const secure = require('./secure.json');
 const Commando = require('discord.js-commando');
 const path = require('path');
-const fs = require('fs');
 
 const Cinnamon = new Commando.Client({
 	owner: ['87723984799399936', '147604925612818432'],
@@ -29,10 +27,3 @@ Cinnamon.on('ready', () => {
 
 // Checks if there's a bot token from Heroku
 if (process.env.BOT_TOKEN) Cinnamon.login(process.env.BOT_TOKEN);
-
-// If no bot token, attempts to load from secure.json
-else if (fs.existsSync('secure.json')) {
-	Cinnamon.login(secure.discordAPIKey);
-} else {
-	console.error('ERROR: No Heroku environment variables or secure.json found!');
-}
