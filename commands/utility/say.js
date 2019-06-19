bf070154b406ed53a8d7a5fd5aab4cd69344a909
Zach Love <abc811@gmail.com>
2019-06-19 17:10:02 -0400

    Say command
     - Bot owners can now get cinnamon to say stuff to channels.

---
+++ commands/utility/say.js
@@ -0,0 +1,36 @@
+const {Command} = require('discord.js-commando');
+
+module.exports = class ImageCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'say',
+			group: 'utility',
+			memberName: 'say',
+			description: '(OWNER ONLY) Sends a message to a channel id',
+			details: '(OWNER ONLY) Sends a message to a channel id',
+			examples: ['say 469339600196993025 what up'],
+			format: '[query]',
+			ownerOnly: true,
+			throttling: {
+				usages: 2,
+				duration: 10
+			},
+			args: [
+				{
+					key: 'channelID',
+					prompt: 'Channel ID to send the message in',
+					type: 'string'
+				},
+				{
+					key: 'channelmsg',
+					prompt: 'Message to send',
+					type: 'string'
+				},
+			]
+		});
+	}
+
+	async run(msg, { channelID, channelmsg }) {
+		return this.client.channels.get(channelID).send(channelmsg);
+	}
+};
