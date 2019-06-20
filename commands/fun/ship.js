582d69bf1e59ada39f84be659ceccaaac596cedc
Zach Love <abc811@gmail.com>
2019-06-20 01:39:23 -0400

    Ship QOL changes
     - Overlay upon >75 match
     - Heart changes opacity based on match

--- commands/fun/ship.js
+++ commands/fun/ship.js
@@ -45,6 +45,7 @@ module.exports = class ShipCommand extends Command {
 		if(shipNum >= 40) reaction = "You can make it work!";
 		if(shipNum >= 65) reaction = "So cute together!";
 		if(shipNum >= 85) reaction = "You two are such a power couple!";
+		if(shipNum == 100) reaction = "It was meant to be!";
 
 		//If you ship with yourself
 		if(user1 == user2) {
@@ -53,7 +54,7 @@ module.exports = class ShipCommand extends Command {
 		}
 
 		// Set a new canvas to the dimensions of 700x300 pixels
-		const canvas = Canvas.createCanvas(700, 300);
+		const canvas = Canvas.createCanvas(750, 300);
 		const ctx = canvas.getContext('2d');
         
 		//Load avatars
@@ -61,18 +62,32 @@ module.exports = class ShipCommand extends Command {
 		const avatar2 = await Canvas.loadImage(user2.displayAvatarURL);
 
 		//Draw avatars
-		ctx.drawImage(avatar1, 0, 0, 300, 300);
-		ctx.drawImage(avatar2, 400, 0, 300, 300);
+		ctx.drawImage(avatar1, 20, 20, 300, 300);
+		ctx.drawImage(avatar2, 420, 20, 300, 300);
 
 		//Draw heart
 		const heart = await Canvas.loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'ship-heart.png'));
-		ctx.drawImage(heart, 250, 50, 200, 200);
+		//Change opacity based on ship num
+		const heartOpacity = shipNum / 100;
+		//Define lower limit to opacity
+		if (heartOpacity <= 0.4) { heartOpacity == 0.4;}
+		ctx.globalAlpha = heartOpacity;
+		//Draw heart and reset opacity
+		ctx.drawImage(heart, 270, 80, 200, 200);
+		ctx.globalAlpha = 1;
+
+		//If match > 75%, add overlay
+		if(shipNum >= 75) {
+			const overlay = await Canvas.loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'ship-overlay.png'));
+			ctx.drawImage(overlay, 0, 0, 320, 110);
+			ctx.drawImage(overlay, 420, 0, 320, 110);
+		}
 
 		//Draw percentage
 		ctx.font = 'bold 60px sans-serif';
 		ctx.textAlign = "center"; 
 		ctx.fillStyle = '#ffffff';
-		ctx.fillText(shipNum + '%', canvas.width / 2, 150);
+		ctx.fillText(shipNum + '%', canvas.width / 2, 180);
     
 
 		//Display
