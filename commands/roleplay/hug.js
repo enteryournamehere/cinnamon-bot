b2061f46535340741d684634f0eaed8049df754c
Zach Love <abc811@gmail.com>
2019-07-06 21:35:45 -0400

    Added hug and kiss commands

---
+++ commands/roleplay/hug.js
@@ -0,0 +1,45 @@
+const {Command} = require('discord.js-commando');
+const path = require('path');
+
+const hugImages = ['hug1.gif', 'hug2.gif', 'hug3.gif']
+
+module.exports = class hugCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'hug',
+			group: 'roleplay',
+			memberName: 'hug',
+			description: 'Hugs a user',
+			details: 'Hugs a user',
+			examples: ['hug @user1'],
+			guildOnly: true,
+			throttling: {
+				usages: 2,
+				duration: 10
+			},
+			args: [
+				{
+					key: 'victim',
+					label: "victim",
+					prompt: 'Which user would you like to hug?',
+					type: 'user'
+				},
+			]
+		});
+	}
+
+	async run(msg, { victim }) {
+
+		const hugSelection = hugImages[Math.floor(Math.random()*hugImages.length)];
+
+		
+        
+		let reaction = "I'll give you a hug, " + msg.author + "!";
+		if(victim != msg.author) {
+			reaction = msg.author + " hugs " + victim + " :)";
+		}
+
+		//Display
+		return msg.say(reaction, { files: [{ attachment: path.join(__dirname, '..', '..', 'assets', 'images', hugSelection), name: 'hug.gif' }] });
+	}
+};
---
+++ commands/roleplay/kiss.js
@@ -0,0 +1,45 @@
+const {Command} = require('discord.js-commando');
+const path = require('path');
+
+const kissImages = ['kiss1.gif', 'kiss2.gif', 'kiss3.gif']
+
+module.exports = class kissCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'kiss',
+			group: 'roleplay',
+			memberName: 'kiss',
+			description: 'Kisses a user',
+			details: 'Kisses a user',
+			examples: ['kiss @user1'],
+			guildOnly: true,
+			throttling: {
+				usages: 2,
+				duration: 10
+			},
+			args: [
+				{
+					key: 'victim',
+					label: "victim",
+					prompt: 'Which user would you like to kiss?',
+					type: 'user'
+				},
+			]
+		});
+	}
+
+	async run(msg, { victim }) {
+
+		const kissSelection = kissImages[Math.floor(Math.random()*kissImages.length)];
+
+		
+        
+		let reaction = "No homo, " + msg.author + " <3";
+		if(victim != msg.author) {
+			reaction = msg.author + " kisses " + victim + " :)";
+		}
+
+		//Display
+		return msg.say(reaction, { files: [{ attachment: path.join(__dirname, '..', '..', 'assets', 'images', kissSelection), name: 'kiss.gif' }] });
+	}
+};
